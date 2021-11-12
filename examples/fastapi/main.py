317c4fa54de683c0849f6db21551be1b4c95752d
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-11-12 19:08:21 +0530

    fastapi example update

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -1,47 +1,192 @@
-from fastapi import FastAPI, Depends
-from starlette.requests import Request
-from supertokens_python import init
-from supertokens_python.recipe import session
-from supertokens_python.framework.fastapi import Middleware
-from supertokens_python.recipe.session.asyncio import create_new_session, refresh_session, Session
-from supertokens_python.recipe.session.framework.fastapi import verify_session
+import os
 
-app = FastAPI()
+import typing
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse, PlainTextResponse
+from starlette.datastructures import Headers
+from starlette.exceptions import ExceptionMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+from supertokens_python import init, get_all_cors_headers
+from supertokens_python.framework.fastapi import Middleware
+from supertokens_python.recipe import session, thirdpartyemailpassword, thirdparty, emailpassword
+from supertokens_python.recipe.session import Session
+from supertokens_python.recipe.session.framework.fastapi import verify_session
+from supertokens_python.recipe.thirdparty import Github, Google, Facebook, Apple
+
+load_dotenv()
+
+app = FastAPI(debug=True)
+app.add_middleware(Middleware)
+os.environ.setdefault('SUPERTOKENS_ENV', 'testing')
+
+
+def get_api_port():
+    return '3001'
+
+
+def get_website_port():
+    return '3000'
+
+
+def get_website_domain():
+    return 'http://localhost:' + get_website_port()
+
+
+latest_url_with_token = None
+
+
+async def create_and_send_custom_email(_, url_with_token):
+    global latest_url_with_token
+    latest_url_with_token = url_with_token
+
+
+async def validate_age(value):
+    try:
+        if int(value) < 18:
+            return "You must be over 18 to register"
+    except Exception:
+        pass
+
+    return None
+
+form_fields = [{
+    'id': 'name'
+}, {
+    'id': 'age',
+    'validate': validate_age
+}, {
+    'id': 'country',
+    'optional': True
+}]
+
 
 init({
     'supertokens': {
-        'connection_uri': "http://localhost:3567",
+        'connection_uri': "https://try.supertokens.io",
     },
     'framework': 'fastapi',
     'app_info': {
-        'app_name': "SuperTokens Demo",
-        'api_domain': "http://127.0.0.1:8000",
-        'website_domain': "http://127.0.0.1:8000",
-        'api_base_path': "/auth"
+        'app_name': "SuperTokens",
+        'api_domain': "0.0.0.0:" + get_api_port(),
+        'website_domain': get_website_domain(),
     },
-    'recipe_list': [session.init(
-        {
-            'anti_csrf': 'VIA_TOKEN',
-            'cookie_domain': '127.0.0.1:8000'
-        }
-    )],
+    'recipe_list': [
+        session.init({}),
+        emailpassword.init({
+            'sign_up_feature': {
+                'form_fields': form_fields
+            },
+            'reset_password_using_token_feature': {
+                'create_and_send_custom_email': create_and_send_custom_email
+            },
+            'email_verification_feature': {
+                'create_and_send_custom_email': create_and_send_custom_email
+            }
+        }),
+        thirdparty.init({
+            'sign_in_and_up_feature': {
+                'providers': [
+                    Google(
+                        client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+                        client_secret=os.environ.get('GOOGLE_CLIENT_SECRET')
+                    ), Facebook(
+                        client_id=os.environ.get('FACEBOOK_CLIENT_ID'),
+                        client_secret=os.environ.get('FACEBOOK_CLIENT_SECRET')
+                    ), Github(
+                        client_id=os.environ.get('GITHUB_CLIENT_ID'),
+                        client_secret=os.environ.get('GITHUB_CLIENT_SECRET')
+                    ), Apple(
+                        client_id=os.environ.get('APPLE_CLIENT_ID'),
+                        client_key_id=os.environ.get('APPLE_KEY_ID'),
+                        client_team_id=os.environ.get('APPLE_TEAM_ID'),
+                        client_private_key=os.environ.get('APPLE_PRIVATE_KEY')
+                    )
+                ]
+            }
+        }),
+        thirdpartyemailpassword.init({
+            'sign_up_feature': {
+                'form_fields': form_fields
+            },
+            'providers': [
+                Google(
+                    client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+                    client_secret=os.environ.get('GOOGLE_CLIENT_SECRET')
+                ), Facebook(
+                    client_id=os.environ.get('FACEBOOK_CLIENT_ID'),
+                    client_secret=os.environ.get('FACEBOOK_CLIENT_SECRET')
+                ), Github(
+                    client_id=os.environ.get('GITHUB_CLIENT_ID'),
+                    client_secret=os.environ.get('GITHUB_CLIENT_SECRET')
+                ), Apple(
+                    client_id=os.environ.get('APPLE_CLIENT_ID'),
+                    client_key_id=os.environ.get('APPLE_KEY_ID'),
+                    client_team_id=os.environ.get('APPLE_TEAM_ID'),
+                    client_private_key=os.environ.get('APPLE_PRIVATE_KEY')
+                )
+            ]
+        })
+    ],
+    'telemetry': False
 })
 
-app.add_middleware(Middleware)
+
+app.add_middleware(ExceptionMiddleware, handlers=app.exception_handlers)
 
 
-@app.get("/create")
-async def create(request: Request):
-    user_id = 'user_id'
-    session = await create_new_session(request, user_id, {}, {})
-    return {"message": session.get_access_token()}
+@app.get('/sessioninfo')
+async def get_session_info(session_: Session = Depends(verify_session())):
+    return JSONResponse({
+        'sessionHandle': session_.get_handle(),
+        'userId': session_.get_user_id(),
+        'accessTokenPayload': session_.get_access_token_payload(),
+        # 'sessionData': await session_.get_session_data()
+    })
 
 
-@app.get("/user")
-async def user(session: Session = Depends(verify_session())):
-    return {'user_id': session.get_user_id()}
+@app.exception_handler(405)
+def f_405(_, e):
+    return PlainTextResponse('', status_code=404)
 
 
-@app.post("/refresh")
-async def refresh(request: Request):
-    await refresh_session(request)
+class CustomCORSMiddleware(CORSMiddleware):
+    def __init__(
+        self,
+        app_: ASGIApp,
+        allow_origins: typing.Sequence[str] = (),
+        allow_methods: typing.Sequence[str] = ("GET",),
+        allow_headers: typing.Sequence[str] = (),
+        allow_credentials: bool = False,
+        allow_origin_regex: str = None,
+        expose_headers: typing.Sequence[str] = (),
+        max_age: int = 600,
+    ) -> None:
+        super().__init__(app_, allow_origins, allow_methods, allow_headers, allow_credentials, allow_origin_regex,
+                         expose_headers, max_age)
+
+    def preflight_response(self, request_headers: Headers) -> Response:
+        result = super().preflight_response(request_headers)
+        if result.status_code == 200:
+            result.headers.__delitem__('content-type')
+            result.headers.__delitem__('content-length')
+            return Response(status_code=204, headers=dict(result.headers))
+        return result
+
+
+app = CustomCORSMiddleware(
+    app_=app,
+    allow_origins=[
+        get_website_domain()
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=["Content-Type"] + get_all_cors_headers(),
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=get_api_port())
