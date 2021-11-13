04a8b164896fb3b0d33aaae8cc55ffaac0a148a1
Rishabh <rishabh@supertokens.io>
2021-11-13 10:16:34 +0530

    fixes tests - maybe

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,6 +11,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-import nest_asyncio
-nest_asyncio.apply()
