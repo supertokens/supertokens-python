d0b7227cc87f805a90a3e7a6bd399cf16894f484
Rishabh <rishabh@supertokens.io>
2021-12-18 02:07:56 +0530

    initial try

--- .circleci/doTests.sh
+++ .circleci/doTests.sh
@@ -21,50 +21,6 @@ while IFS='"' read -ra ADDR; do
     done
 done <<< "$version"
 
-responseStatus=`curl -s -o /dev/null -w "%{http_code}" -X PUT \
-  https://api.supertokens.io/0/driver \
-  -H 'Content-Type: application/json' \
-  -H 'api-version: 0' \
-  -d "{
-	\"password\": \"$SUPERTOKENS_API_KEY\",
-	\"version\":\"$version\",
-    \"name\": \"python\",
-	\"frontendDriverInterfaces\": $frontendDriverArray,
-	\"coreDriverInterfaces\": $coreDriverArray
-}"`
-if [ $responseStatus -ne "200" ]
-then
-    echo "failed core PUT API status code: $responseStatus. Exiting!"
-	exit 1
-fi
-
-someTestsRan=false
-i=0
-while [ $i -lt $coreDriverLength ]; do
-    coreDriverVersion=`echo $coreDriverArray | jq ".[$i]"`
-    coreDriverVersion=`echo $coreDriverVersion | tr -d '"'`
-    i=$((i+1))
-
-    coreFree=`curl -s -X GET \
-    "https://api.supertokens.io/0/core-driver-interface/dependency/core/latest?password=$SUPERTOKENS_API_KEY&planType=FREE&mode=DEV&version=$coreDriverVersion" \
-    -H 'api-version: 0'`
-    if [[ `echo $coreFree | jq .core` == "null" ]]
-    then
-        echo "fetching latest X.Y version for core given core-driver-interface X.Y version: $coreDriverVersion, planType: FREE gave response: $coreFree. Please make sure all relevant cores have been pushed."
-        exit 1
-    fi
-    coreFree=$(echo $coreFree | jq .core | tr -d '"')
-
-    someTestsRan=true
-    ./setupAndTestWithFreeCore.sh $coreFree $coreDriverVersion
-    if [[ $? -ne 0 ]]
-    then
-        echo "test failed... exiting!"
-        exit 1
-    fi
-    rm -rf ../../supertokens-root
-done
-
 someFrontendTestsRan=false
 i=0
 coreDriverVersion=`echo $coreDriverArray | jq ". | last"`
@@ -297,26 +253,3 @@ while [ $i -lt $frontendDriverLength ]; do
     fi
 
 done
-
-if [[ $someFrontendTestsRan = "true" ]] && [[ $someTestsRan = "true" ]]
-then
-    echo "calling /driver PATCH to make testing passed"
-    responseStatus=`curl -s -o /dev/null -w "%{http_code}" -X PATCH \
-        https://api.supertokens.io/0/driver \
-        -H 'Content-Type: application/json' \
-        -H 'api-version: 0' \
-        -d "{
-            \"password\": \"$SUPERTOKENS_API_KEY\",
-            \"version\":\"$version\",
-            \"name\": \"python\",
-            \"testPassed\": true
-        }"`
-    if [ $responseStatus -ne "200" ]
-    then
-        echo "patch api failed"
-        exit 1
-    fi
-else
-    echo "no test ran"
-    exit 1
-fi
