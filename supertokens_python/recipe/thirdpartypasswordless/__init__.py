ba9ee2dce678be793e604f58cc3e830da7877c40
Rishabh <rishabh@supertokens.io>
2022-03-04 15:11:44 +0530

    exposes user functions

--- supertokens_python/recipe/thirdpartypasswordless/__init__.py
+++ supertokens_python/recipe/thirdpartypasswordless/__init__.py
@@ -11,40 +11,43 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-# from __future__ import annotations
+from __future__ import annotations
 
-# from typing import TYPE_CHECKING, Callable, List, Union
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict, List, Union
 
-# from supertokens_python.recipe.thirdparty.provider import Provider
+from supertokens_python.recipe.thirdparty.provider import Provider
+from typing_extensions import Literal
 
-# from .. import emailpassword, thirdparty
-# from . import exceptions as ex
-# from . import utils
-# from .recipe import ThirdPartyEmailPasswordRecipe
+from .. import passwordless, thirdparty
+from . import exceptions as ex
+from . import utils
+from .recipe import ThirdPartyPasswordlessRecipe
 
-# InputEmailVerificationConfig = utils.InputEmailVerificationConfig
-# InputOverrideConfig = utils.InputOverrideConfig
-# exceptions = ex
-# InputResetPasswordUsingTokenFeature = emailpassword.InputResetPasswordUsingTokenFeature
-# InputSignUpFeature = emailpassword.InputSignUpFeature
-# Apple = thirdparty.Apple
-# Discord = thirdparty.Discord
-# Facebook = thirdparty.Facebook
-# Github = thirdparty.Github
-# Google = thirdparty.Google
-# GoogleWorkspaces = thirdparty.GoogleWorkspaces
+InputEmailVerificationConfig = utils.InputEmailVerificationConfig
+InputOverrideConfig = utils.InputOverrideConfig
+exceptions = ex
+ContactConfig = passwordless.ContactConfig
+PhoneOrEmailInput = passwordless.PhoneOrEmailInput
+Apple = thirdparty.Apple
+Discord = thirdparty.Discord
+Facebook = thirdparty.Facebook
+Github = thirdparty.Github
+Google = thirdparty.Google
+GoogleWorkspaces = thirdparty.GoogleWorkspaces
 
-# if TYPE_CHECKING:
-#     from supertokens_python.supertokens import AppInfo
+if TYPE_CHECKING:
+    from supertokens_python.supertokens import AppInfo
 
-#     from ...recipe_module import RecipeModule
+    from ...recipe_module import RecipeModule
 
 
-# def init(sign_up_feature: Union[InputSignUpFeature, None] = None,
-#          reset_password_using_token_feature: Union[InputResetPasswordUsingTokenFeature, None] = None,
-#          email_verification_feature: Union[InputEmailVerificationConfig, None] = None,
-#          override: Union[InputOverrideConfig, None] = None,
-#          providers: Union[List[Provider], None] = None) -> Callable[[AppInfo], RecipeModule]:
-#     return ThirdPartyEmailPasswordRecipe.init(sign_up_feature, reset_password_using_token_feature,
-#                                               email_verification_feature,
-#                                               override, providers)
+def init(contact_config: ContactConfig,
+         flow_type: Literal['USER_INPUT_CODE', 'MAGIC_LINK', 'USER_INPUT_CODE_AND_MAGIC_LINK'],
+         get_link_domain_and_path: Union[Callable[[
+             PhoneOrEmailInput, Dict[str, Any]], Awaitable[str]], None] = None,
+         get_custom_user_input_code: Union[Callable[[Dict[str, Any]], Awaitable[str]], None] = None,
+         email_verification_feature: Union[InputEmailVerificationConfig, None] = None,
+         override: Union[InputOverrideConfig, None] = None,
+         providers: Union[List[Provider], None] = None) -> Callable[[AppInfo], RecipeModule]:
+    return ThirdPartyPasswordlessRecipe.init(contact_config, flow_type, get_link_domain_and_path, get_custom_user_input_code, email_verification_feature,
+                                             override, providers)
