5c1eea85876da5d2d17b37c0b5ae5fedfc28c952
KShivendu <shivendu@iitbhilai.ac.in>
2022-03-15 15:07:12 +0530

    feat: Add license and fix quotation

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ exclude_list = [
 
 setup(
     name="supertokens_python",
-    version='0.5.1',
+    version="0.5.1",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.io",
