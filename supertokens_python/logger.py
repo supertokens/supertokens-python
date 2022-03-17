44fb10583ee356087f75e4cd594be0c26d5a0534
KShivendu <shivendu@iitbhilai.ac.in>
2022-03-17 22:34:52 +0530

    feat: Improve logging logic and format

--- supertokens_python/logger.py
+++ supertokens_python/logger.py
@@ -14,75 +14,60 @@
 
 import json
 import logging
-import logging.config
 from datetime import datetime
+from math import floor
 from os import getenv
-from typing import Any, Callable, Dict, Union
+from typing import TextIO, Union
 
 from .constants import VERSION
 
-
-class LoggerCodes:
-    API_RESPONSE = 1
-
-# Setup level names to match log output with other st libraries
-# If this step is removed, the output will print 'DEBUG' instead of 'debug'
-level_maps = {
-    logging.DEBUG: "debug",
-    logging.INFO: "info",
-}
-for level, alias in level_maps.items():
-    logging.addLevelName(level, alias)
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
 
 # Configure logger
-logger = logging.getLogger("com.supertokens")
-log_level = getenv('LOG_LEVEL', "").lower()
-map = {"debug": logging.DEBUG, "info": logging.INFO}
-logger.setLevel(map.get(log_level, logging.INFO))
+logger = logging.getLogger()
+log_level_str = getenv('LOG_LEVEL', "").lower()
+default_log_level = logging.INFO
+log_level_dict = {"debug": logging.DEBUG, "info": logging.INFO}
+
+logger.setLevel(log_level_dict.get(log_level_str, default_log_level))
+
+
+def _get_log_timestamp():
+    return floor(datetime.timestamp(datetime.now()))
+
+
+class CustomStreamHandler(logging.StreamHandler):  # type: ignore
+    def __init__(self, stream: Union[TextIO, None] = None):
+        super().__init__(stream)  # type: ignore
+        self.last = .0
+
+    def transform(self, record: logging.LogRecord) -> None:
+        record.msg = json.dumps({
+            "t": _get_log_timestamp(),
+            "sdkVer": VERSION,
+            "message": record.msg,
+            "file": f'{record.pathname}:{record.lineno}'
+        })
+
+        # Adding relative difference wrt the prev log
+        try:
+            last = self.last
+        except AttributeError:
+            last = record.relativeCreated
+
+        delta = datetime.fromtimestamp(record.relativeCreated / 1000.0) - datetime.fromtimestamp(last / 1000.0)
+        record.relative = f"{(delta.seconds + delta.microseconds / 1000000.0):.2f}"  # type: ignore
+        self.last = record.relativeCreated
+
+    def emit(self, record: logging.LogRecord):
+        self.transform(record)
+        return super().emit(record)
+
 
 # Add stream handler and format
-streamHandler = logging.StreamHandler()
-stream_formatter = logging.Formatter("{name}:{levelname} {message}", style="{")
-streamHandler.setFormatter(stream_formatter)
+streamHandler = CustomStreamHandler()
+streamFormatter = logging.Formatter("com.supertokens {message} +{relative}ms", style="{")
+streamHandler.setFormatter(streamFormatter)
+# streamHandler.addFilter(TimeFilter())
 logger.addHandler(streamHandler)
-
-
-def _get_iso_date():
-    return datetime.now
-
-
-def _log_message(
-    log_level: int, extra_params: Union[None, Dict[str, Any]] = None
-):
-    if extra_params is None:
-        extra_params = {}
-
-    json_msg = json.dumps({
-        "t": _get_iso_date(),
-        "sdkVer": VERSION,
-        **extra_params,
-    })
-
-    if log_level == logging.INFO:
-        logger.info(json_msg)
-    elif log_level == logging.DEBUG:
-        logger.debug(json_msg)
-
-
-def _debug_logger_helper(debug_code: int, message: str):
-    _log_message(logging.DEBUG, {"message": message, "debugCode": debug_code})
-
-
-def info_logger(message: str):
-    _log_message(logging.INFO, {"message": message})
-
-
-def debug_logger(item: str, code: int):
-    def api_response(item: str) -> str:
-        return f"API replied with status {item}"
-    
-    code_to_msg: Dict[int, Callable[..., str]] = {
-        LoggerCodes.API_RESPONSE: api_response
-    }
-    _debug_logger_helper(code, code_to_msg[code](item))
-
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,33 +1,22 @@
 import json
+import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
 from supertokens_python.constants import VERSION
-from supertokens_python.logger import LoggerCodes, debug_logger, info_logger
+from supertokens_python.logger import streamHandler
 
 
 class LoggerUtilsTests(TestCase):
-    @patch('supertokens_python.logger._get_iso_date', return_value='iso_date')
-    def test_debug_logger_func(self, mock_iso_date: MagicMock):
-        with self.assertLogs('com.supertokens', level='DEBUG') as captured:
-            debug_logger('foo', LoggerCodes.API_RESPONSE)
+    @patch('supertokens_python.logger._get_log_timestamp', return_value='timestamp')
+    def test_debug_logger_func(self, mock_timestamp: MagicMock):  # pylint: disable=unused-argument
+        with self.assertLogs(level='DEBUG') as captured:
+            logging.info("API replied with status 200")
 
-        assert len(captured.records) == 1
-        assert captured.records[0].getMessage() == json.dumps({
-            "t": "iso_date",
-            "sdkVer": VERSION,
-            "message": "API replied with status foo",
-            "debugCode": 1
-        })
+        record = captured.records[0]
+        streamHandler.transform(record)
+        out = json.loads(record.msg)
+        filename = out.pop('file').split(':')[0]
 
-    @patch('supertokens_python.logger._get_iso_date', return_value='iso_date')
-    def test_info_logger_func(self, mock_iso_date: MagicMock):
-        with self.assertLogs('com.supertokens', level='INFO') as captured:
-            info_logger('bar')
-
-        assert len(captured.records) == 1
-        assert captured.records[0].getMessage() == json.dumps({
-            "t": "iso_date",
-            "sdkVer": VERSION,
-            "message": "bar",
-        })
+        assert filename.endswith('tests/test_logger.py')
+        assert out == {'t': 'timestamp', 'sdkVer': VERSION, 'message': 'API replied with status 200'}
