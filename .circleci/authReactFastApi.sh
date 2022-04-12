0eef7937d330703283770d9729e68ae8cbb08474
rishabhpoddar <rishabh.poddar@gmail.com>
2022-04-12 13:38:25 +0530

    initial try

--- .circleci/authReactFastApi.sh
+++ .circleci/authReactFastApi.sh
@@ -7,8 +7,6 @@ fi
 frontendDriverVersion=$1
 frontendDriverVersion=`echo $frontendDriverVersion | tr -d '"'`
 if [[ $frontendDriverVersion == '1.0' ]]; then
-    mkdir ../../supertokens-auth-react
-    mkdir ../../supertokens-auth-react/test_report
     exit 0
 fi
 
@@ -94,8 +92,6 @@ if [[ $frontendDriverVersion == '1.3' || $frontendDriverVersion == '1.8' ]]; the
     # we skip this since the tests for auth-react here are not reliable due to race conditions...
 
     # we skip 1.8 since the SDK with just 1.8 doesn't have the right scripts
-    mkdir ../../supertokens-auth-react
-    mkdir ../../supertokens-auth-react/test_report
     exit 0
 else
     tries=1
