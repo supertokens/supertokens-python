78a29d276335e8248b73855ff686de36d065b997
rishabhpoddar <rishabh.poddar@gmail.com>
2022-04-23 19:32:44 +0530

    updates version and changelog

--- supertokens_python/constants.py
+++ supertokens_python/constants.py
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 SUPPORTED_CDI_VERSIONS = ['2.9', '2.10', '2.11', '2.12', '2.13']
-VERSION = '0.6.6'
+VERSION = '0.6.7'
 TELEMETRY = '/telemetry'
 USER_COUNT = '/users/count'
 USER_DELETE = '/user/remove'
