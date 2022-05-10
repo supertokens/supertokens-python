2fa2a166c5991dd04f7118373645d7dfa905d838
Sattvik Chakravarthy <sattvik@gmail.com>
2022-05-10 11:02:21 +0530

    fix: adding tests

--- tests/input_validation/test_input_validation.py
+++ tests/input_validation/test_input_validation.py
@@ -1,7 +1,93 @@
 import pytest
 from typing import Dict, Any
 from supertokens_python import InputAppInfo, SupertokensConfig, init
-from supertokens_python.recipe import passwordless
+from supertokens_python.recipe import emailpassword, passwordless
+
+
+@pytest.mark.asyncio
+async def test_init_validation_emailpassword():
+    with pytest.raises(ValueError) as ex:
+        init(
+            supertokens_config=SupertokensConfig('http://localhost:3567'),
+            app_info='AppInfo',  # type: ignore
+            framework='fastapi',
+            recipe_list=[
+                emailpassword.init(),
+            ]
+        )
+    assert 'app_info must be an instance of InputAppInfo' == str(ex.value)
+
+    with pytest.raises(ValueError) as ex:
+        init(
+            supertokens_config=SupertokensConfig('http://localhost:3567'),
+            app_info=InputAppInfo(
+                app_name="SuperTokens Demo",
+                api_domain="http://api.supertokens.io",
+                website_domain="http://supertokens.io",
+                api_base_path="/auth"
+            ),
+            framework='fastapi',
+            recipe_list=[
+                emailpassword.init(
+                    sign_up_feature='sign up'  # type: ignore
+                ),
+            ]
+        )
+    assert 'sign_up_feature must be of type InputSignUpFeature or None' == str(ex.value)
+
+    with pytest.raises(ValueError) as ex:
+        init(
+            supertokens_config=SupertokensConfig('http://localhost:3567'),
+            app_info=InputAppInfo(
+                app_name="SuperTokens Demo",
+                api_domain="http://api.supertokens.io",
+                website_domain="http://supertokens.io",
+                api_base_path="/auth"
+            ),
+            framework='fastapi',
+            recipe_list=[
+                emailpassword.init(
+                    reset_password_using_token_feature='reset password'  # type: ignore
+                ),
+            ]
+        )
+    assert 'reset_password_using_token_feature must be of type InputResetPasswordUsingTokenFeature or None' == str(ex.value)
+
+    with pytest.raises(ValueError) as ex:
+        init(
+            supertokens_config=SupertokensConfig('http://localhost:3567'),
+            app_info=InputAppInfo(
+                app_name="SuperTokens Demo",
+                api_domain="http://api.supertokens.io",
+                website_domain="http://supertokens.io",
+                api_base_path="/auth"
+            ),
+            framework='fastapi',
+            recipe_list=[
+                emailpassword.init(
+                    email_verification_feature='email verify'  # type: ignore
+                ),
+            ]
+        )
+    assert 'email_verification_feature must be of type InputEmailVerificationConfig or None' == str(ex.value)
+
+    with pytest.raises(ValueError) as ex:
+        init(
+            supertokens_config=SupertokensConfig('http://localhost:3567'),
+            app_info=InputAppInfo(
+                app_name="SuperTokens Demo",
+                api_domain="http://api.supertokens.io",
+                website_domain="http://supertokens.io",
+                api_base_path="/auth"
+            ),
+            framework='fastapi',
+            recipe_list=[
+                emailpassword.init(
+                    override='override'  # type: ignore
+                ),
+            ]
+        )
+    assert 'override must be of type InputOverrideConfig or None' == str(ex.value)
 
 
 async def send_text_message(param: passwordless.CreateAndSendCustomTextMessageParameters, _: Dict[str, Any]):
