ca2775356ce19b80e992ee14145ed741f3813bb7
Sattvik Chakravarthy <sattvik@gmail.com>
2022-05-13 09:59:09 +0530

    chore: updated version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ exclude_list = [
 
 setup(
     name="supertokens_python",
-    version="0.8.1",
+    version="0.8.2",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.com",
