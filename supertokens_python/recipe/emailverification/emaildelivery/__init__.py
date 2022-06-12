c87431097239307b99748f0937ad76c196d0a190
KShivendu <shivendu@iitbhilai.ac.in>
2022-06-12 08:41:39 +0530

    Export services from ingredient/services instead of ingredient

--- supertokens_python/recipe/emailverification/emaildelivery/__init__.py
+++ supertokens_python/recipe/emailverification/emaildelivery/__init__.py
@@ -11,7 +11,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from .services.smtp import EmailDeliverySMTPConfig, SMTPService
-
-_ = EmailDeliverySMTPConfig
-_ = SMTPService
