f35003ca63ae0b3503dcd238dc969eeff62c20aa
KShivendu <shivendu@iitbhilai.ac.in>
2022-09-09 13:40:12 +0530

    feat: last changes

--- supertokens_python/recipe/userroles/recipe.py
+++ supertokens_python/recipe/userroles/recipe.py
@@ -15,7 +15,7 @@
 from __future__ import annotations
 
 from os import environ
-from typing import List, Union
+from typing import List, Union, Optional, Dict, Any, Set
 
 from supertokens_python.exceptions import SuperTokensError, raise_general_exception
 from supertokens_python.framework import BaseRequest, BaseResponse
@@ -29,7 +29,11 @@ from supertokens_python.recipe_module import APIHandled, RecipeModule
 from supertokens_python.supertokens import AppInfo
 
 from .exceptions import SuperTokensUserRolesError
+from .interfaces import GetPermissionsForRoleOkResult
 from .utils import InputOverrideConfig
+from ..session import SessionRecipe
+from ..session.claim_base_classes.primitive_array_claim import PrimitiveArrayClaim
+from ...post_init_callbacks import PostSTInitCallbacks
 
 
 class UserRolesRecipe(RecipeModule):
@@ -40,16 +44,34 @@ class UserRolesRecipe(RecipeModule):
         self,
         recipe_id: str,
         app_info: AppInfo,
+        skip_adding_roles_to_access_token: Optional[bool] = None,
+        skip_adding_permissions_to_access_token: Optional[bool] = None,
         override: Union[InputOverrideConfig, None] = None,
     ):
         super().__init__(recipe_id, app_info)
-        self.config = validate_and_normalise_user_input(self, app_info, override)
+        self.config = validate_and_normalise_user_input(
+            self,
+            app_info,
+            skip_adding_roles_to_access_token,
+            skip_adding_permissions_to_access_token,
+            override,
+        )
         recipe_implementation = RecipeImplementation(Querier.get_instance(recipe_id))
         self.recipe_implementation = (
             recipe_implementation
             if self.config.override.functions is None
             else self.config.override.functions(recipe_implementation)
         )
+
+        def callback():
+            if self.config.skip_adding_roles_to_access_token is False:
+                SessionRecipe.get_instance().add_claim_from_other_recipe(UserRoleClaim)
+            if self.config.skip_adding_permissions_to_access_token is False:
+                SessionRecipe.get_instance().add_claim_from_other_recipe(
+                    PermissionClaim
+                )
+
+        PostSTInitCallbacks.add_post_init_callback(callback)
 
     def is_error_from_this_recipe_based_on_instance(self, err: Exception) -> bool:
         return isinstance(err, SuperTokensError) and (
@@ -78,11 +100,19 @@ class UserRolesRecipe(RecipeModule):
         return []
 
     @staticmethod
-    def init(override: Union[InputOverrideConfig, None] = None):
+    def init(
+        skip_adding_roles_to_access_token: Optional[bool] = None,
+        skip_adding_permissions_to_access_token: Optional[bool] = None,
+        override: Union[InputOverrideConfig, None] = None,
+    ):
         def func(app_info: AppInfo):
             if UserRolesRecipe.__instance is None:
                 UserRolesRecipe.__instance = UserRolesRecipe(
-                    UserRolesRecipe.recipe_id, app_info, override
+                    UserRolesRecipe.recipe_id,
+                    app_info,
+                    skip_adding_roles_to_access_token,
+                    skip_adding_permissions_to_access_token,
+                    override,
                 )
                 return UserRolesRecipe.__instance
             raise Exception(
@@ -107,3 +137,52 @@ class UserRolesRecipe(RecipeModule):
         raise_general_exception(
             "Initialisation not done. Did you forget to call the SuperTokens.init or UserRoles.init function?"
         )
+
+
+class PermissionClaimClass(PrimitiveArrayClaim[List[str]]):
+    def __init__(self) -> None:
+        key = "st-perm"
+
+        async def fetch_value(user_id: str, user_context: Dict[str, Any]) -> List[str]:
+            recipe = UserRolesRecipe.get_instance()
+
+            user_roles = await recipe.recipe_implementation.get_roles_for_user(
+                user_id, user_context
+            )
+
+            user_permissions: Set[str] = set()
+
+            for role in user_roles.roles:
+                role_permissions = (
+                    await recipe.recipe_implementation.get_permissions_for_role(
+                        role, user_context
+                    )
+                )
+
+                if isinstance(role_permissions, GetPermissionsForRoleOkResult):
+                    for permission in role_permissions.permissions:
+                        user_permissions.add(permission)
+
+            return list(user_permissions)
+
+        super().__init__(key, fetch_value)
+
+
+PermissionClaim = PermissionClaimClass()
+
+
+class UserRoleClaimClass(PrimitiveArrayClaim[List[str]]):
+    def __init__(self) -> None:
+        key = "st-role"
+
+        async def fetch_value(user_id: str, user_context: Dict[str, Any]) -> List[str]:
+            recipe = UserRolesRecipe.get_instance()
+            res = await recipe.recipe_implementation.get_roles_for_user(
+                user_id, user_context
+            )
+            return res.roles
+
+        super().__init__(key, fetch_value)
+
+
+UserRoleClaim = UserRoleClaimClass()
