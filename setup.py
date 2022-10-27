7b3393e9bfe2daa1899bd6ec9b0fdcc12ed687e3
KShivendu <shivendu@iitbhilai.ac.in>
2022-10-27 18:45:32 +0530

    chores: Bump version to 0.11.5

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ exclude_list = [
 
 setup(
     name="supertokens_python",
-    version="0.11.4",
+    version="0.11.5",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.com",
