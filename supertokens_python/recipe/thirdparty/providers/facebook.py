8a729a9d0d91e3c514d51116d24323b0e6606bd3
Sattvik Chakravarthy <sattvik@gmail.com>
2023-01-31 16:33:39 +0530

    fix: fb, github, google, okta impl

--- supertokens_python/recipe/thirdparty/providers/facebook.py
+++ supertokens_python/recipe/thirdparty/providers/facebook.py
@@ -41,7 +41,9 @@ class FacebookImpl(GenericProvider):
 
         return config
 
-    async def get_user_info(self, oauth_tokens: Dict[str, Any], user_context: Dict[str, Any]) -> UserInfo:
+    async def get_user_info(
+        self, oauth_tokens: Dict[str, Any], user_context: Dict[str, Any]
+    ) -> UserInfo:
         self.config.user_info_endpoint_query_params = {
             "access_token": str(oauth_tokens["access_token"]),
             "fields": "id,email",
@@ -54,13 +56,17 @@ class FacebookImpl(GenericProvider):
 def Facebook(input: ProviderInput) -> Provider:
     if input.config.name is None:
         input.config.name = "Facebook"
-        
+
     if input.config.authorization_endpoint is None:
-        input.config.authorization_endpoint = "https://www.facebook.com/v12.0/dialog/oauth"
-    
+        input.config.authorization_endpoint = (
+            "https://www.facebook.com/v12.0/dialog/oauth"
+        )
+
     if input.config.token_endpoint is None:
-        input.config.token_endpoint = "https://graph.facebook.com/v12.0/oauth/access_token"
-    
+        input.config.token_endpoint = (
+            "https://graph.facebook.com/v12.0/oauth/access_token"
+        )
+
     if input.config.user_info_endpoint is None:
         input.config.user_info_endpoint = "https://graph.facebook.com/me"
 
--- supertokens_python/recipe/thirdparty/providers/google.py
+++ supertokens_python/recipe/thirdparty/providers/google.py
@@ -12,99 +12,64 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 from __future__ import annotations
+from typing import Any, Callable, Dict, Optional
 
-from typing import TYPE_CHECKING, Any, Callable, Dict, List, Union
-
-from httpx import AsyncClient
-from supertokens_python.recipe.thirdparty.provider import Provider
-from supertokens_python.recipe.thirdparty.types import (
-    AccessTokenAPI,
-    AuthorisationRedirectAPI,
-    UserInfo,
-    UserInfoEmail,
+from ..provider import (
+    Provider,
+    ProviderConfig,
+    ProviderConfigForClientType,
+    ProviderInput,
+    UserFields,
+    UserInfoMap,
 )
 
-if TYPE_CHECKING:
-    from supertokens_python.framework.request import BaseRequest
+from .custom import (
+    GenericProvider,
+    NewProvider,
+)
 
 
-class Google(Provider):
-    def __init__(
-        self,
-        client_id: str,
-        client_secret: str,
-        scope: Union[None, List[str]] = None,
-        authorisation_redirect: Union[
-            None, Dict[str, Union[str, Callable[[BaseRequest], str]]]
-        ] = None,
-        is_default: bool = False,
-    ):
-        super().__init__("google", is_default)
-        default_scopes = ["https://www.googleapis.com/auth/userinfo.email"]
-        if scope is None:
-            scope = default_scopes
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.scopes = list(set(scope))
-        self.access_token_api_url = "https://oauth2.googleapis.com/token"
-        self.authorisation_redirect_url = "https://accounts.google.com/o/oauth2/v2/auth"
-        self.authorisation_redirect_params = {}
-        if authorisation_redirect is not None:
-            self.authorisation_redirect_params = authorisation_redirect
+class GoogleImpl(GenericProvider):
+    async def get_config_for_client_type(
+        self, client_type: Optional[str], user_context: Dict[str, Any]
+    ) -> ProviderConfigForClientType:
+        config = await super().get_config_for_client_type(client_type, user_context)
 
-    async def get_profile_info(
-        self, auth_code_response: Dict[str, Any], user_context: Dict[str, Any]
-    ) -> UserInfo:
-        access_token: str = auth_code_response["access_token"]
-        params = {"alt": "json"}
-        headers = {"Authorization": "Bearer " + access_token}
-        async with AsyncClient() as client:
-            response = await client.get(  # type: ignore
-                url="https://www.googleapis.com/oauth2/v1/userinfo",
-                params=params,
-                headers=headers,
-            )
-            user_info = response.json()
-            user_id = user_info["id"]
-            if "email" not in user_info or user_info["email"] is None:
-                return UserInfo(user_id)
-            is_email_verified = (
-                user_info["verified_email"] if "verified_email" in user_info else False
-            )
-            return UserInfo(
-                user_id, UserInfoEmail(user_info["email"], is_email_verified)
-            )
+        if config.scope is None:
+            config.scope = ["openid", "email"]
 
-    def get_authorisation_redirect_api_info(
-        self, user_context: Dict[str, Any]
-    ) -> AuthorisationRedirectAPI:
-        params = {
-            "scope": " ".join(self.scopes),
-            "response_type": "code",
-            "client_id": self.client_id,
-            "access_type": "offline",
-            "include_granted_scopes": "true",
-            **self.authorisation_redirect_params,
-        }
-        return AuthorisationRedirectAPI(self.authorisation_redirect_url, params)
+        return config
 
-    def get_access_token_api_info(
-        self,
-        redirect_uri: str,
-        auth_code_from_request: str,
-        user_context: Dict[str, Any],
-    ) -> AccessTokenAPI:
-        params = {
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "grant_type": "authorization_code",
-            "code": auth_code_from_request,
-            "redirect_uri": redirect_uri,
-        }
-        return AccessTokenAPI(self.access_token_api_url, params)
 
-    def get_redirect_uri(self, user_context: Dict[str, Any]) -> Union[None, str]:
-        return None
+def Google(
+    input: ProviderInput,
+    base_class: Callable[[ProviderConfig], GoogleImpl] = GoogleImpl,
+) -> Provider:
+    if input.config.name is None:
+        input.config.name = "Google"
 
-    def get_client_id(self, user_context: Dict[str, Any]) -> str:
-        return self.client_id
+    if input.config.oidc_discovery_endpoint is None:
+        input.config.oidc_discovery_endpoint = "https://accounts.google.com/"
+
+    if input.config.user_info_map is None:
+        input.config.user_info_map = UserInfoMap(UserFields(), UserFields())
+
+    if input.config.user_info_map.from_user_info_api.user_id is None:
+        input.config.user_info_map.from_user_info_api.user_id = "id"
+
+    if input.config.user_info_map.from_user_info_api.email is None:
+        input.config.user_info_map.from_user_info_api.email = "email"
+
+    if input.config.user_info_map.from_user_info_api.email_verified is None:
+        input.config.user_info_map.from_user_info_api.email = "email_verified"
+
+    if input.config.authorization_endpoint_query_params is None:
+        input.config.authorization_endpoint_query_params = {}
+
+    input.config.authorization_endpoint_query_params = {
+        "included_grant_scopes": "true",
+        "access_type": "offline",
+        **input.config.authorization_endpoint_query_params,
+    }
+
+    return NewProvider(input, base_class)
