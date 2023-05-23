8e75570f5d62320e2f5fc54117f7bbe694c606aa
Nemi Shah <nemishah1212@gmail.com>
2023-05-23 15:00:21 +0530

    Add helper function to get the original request from user context provided to APIs and functions

--- supertokens_python/asyncio/__init__.py
+++ supertokens_python/asyncio/__init__.py
@@ -11,18 +11,19 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from typing import List, Union, Optional, Dict
+from typing import Any, Dict, List, Optional, Union
 
 from supertokens_python import Supertokens
+from supertokens_python.framework.request import BaseRequest
 from supertokens_python.interfaces import (
     CreateUserIdMappingOkResult,
+    DeleteUserIdMappingOkResult,
+    GetUserIdMappingOkResult,
+    UnknownMappingError,
     UnknownSupertokensUserIDError,
+    UpdateOrDeleteUserIdMappingInfoOkResult,
     UserIdMappingAlreadyExistsError,
     UserIDTypes,
-    UnknownMappingError,
-    GetUserIdMappingOkResult,
-    DeleteUserIdMappingOkResult,
-    UpdateOrDeleteUserIdMappingInfoOkResult,
 )
 from supertokens_python.types import UsersResponse
 
@@ -97,3 +98,9 @@ async def update_or_delete_user_id_mapping_info(
     return await Supertokens.get_instance().update_or_delete_user_id_mapping_info(
         user_id, user_id_type, external_user_id_info
     )
+
+
+def get_request_from_user_context(
+    user_context: Optional[Dict[str, Any]],
+) -> Optional[BaseRequest]:
+    return Supertokens.get_instance().get_request_from_user_context(user_context)
