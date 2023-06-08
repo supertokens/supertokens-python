74c4ec694dc82271b00bc584da0222c41032839c
iresharma <iresh.sharma8@gmail.com>
2023-06-08 12:15:14 +0530

    made anti_csrf_chcek optional in get_session_without_request_respinse

--- supertokens_python/recipe/session/asyncio/__init__.py
+++ supertokens_python/recipe/session/asyncio/__init__.py
@@ -309,7 +309,7 @@ async def get_session(
 
 async def get_session_without_request_response(
     access_token: str,
-    anti_csrf_check: bool,
+    anti_csrf_check: Optional[bool] = None,
     anti_csrf_token: Optional[str] = None,
     session_required: Optional[bool] = None,
     check_database: Optional[bool] = None,
