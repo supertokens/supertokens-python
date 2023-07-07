9baaa3b3a030cffbd5e553cc60f4d9899e75bc30
rishabhpoddar <rishabh.poddar@gmail.com>
2023-07-07 22:47:07 +0530

    updates changelog

--- supertokens_python/constants.py
+++ supertokens_python/constants.py
@@ -14,7 +14,7 @@
 from __future__ import annotations
 
 SUPPORTED_CDI_VERSIONS = ["2.21"]
-VERSION = "0.14.7"
+VERSION = "0.14.8"
 TELEMETRY = "/telemetry"
 USER_COUNT = "/users/count"
 USER_DELETE = "/user/remove"
