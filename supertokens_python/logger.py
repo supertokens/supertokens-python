17fa0cdecd5185d9d89e1e49fe9d14ec6856313f
Benjamin Pereto <benjamin.pereto@switch.ch>
2024-04-08 23:56:23 +0200

    cleanup: remove change for debug logging

--- supertokens_python/logger.py
+++ supertokens_python/logger.py
@@ -35,8 +35,6 @@ debug_env = getenv(DEBUG_ENV_VAR, "").lower()
 if debug_env == "1":
     enable_debug_logging()
 
-enable_debug_logging()
-
 def _get_log_timestamp() -> str:
     return datetime.utcnow().isoformat()[:-3] + "Z"
 
