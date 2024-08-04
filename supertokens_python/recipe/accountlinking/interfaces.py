37846d548c3d1c16dc58431ff4080ba5e9648811
Satyam Kumar <io.satyamtg@gmail.com>
2024-08-04 12:48:56 +0530

    Undo requirements settings changes

--- supertokens_python/recipe/accountlinking/interfaces.py
+++ supertokens_python/recipe/accountlinking/interfaces.py
@@ -14,7 +14,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Union, Optional
 
 from typing_extensions import Literal
 
