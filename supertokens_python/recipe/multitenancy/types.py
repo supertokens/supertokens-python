82a169217cbb1681ac9d24baefa7b9cf3c51a1a0
Satyam Kumar <io.satyamtg@gmail.com>
2024-08-04 12:55:09 +0530

    Minor fixes

--- supertokens_python/recipe/multitenancy/types.py
+++ supertokens_python/recipe/multitenancy/types.py
@@ -1,18 +1,4 @@
-from typing import Any, Dict, List, Literal, Optional
-
-
-class TenantConfig:
-    def __init__(
-        self,
-        third_party: Dict[str, List[Dict[str, str]]],
-        core_config: Dict[str, Any],
-        first_factors: Optional[List[str]] = None,
-        required_secondary_factors: Optional[List[str]] = None,
-    ):
-        self.third_party = third_party
-        self.core_config = core_config
-        self.first_factors = first_factors
-        self.required_secondary_factors = required_secondary_factors
+from typing import Literal
 
 
 class ValidFirstFactorResponse:
