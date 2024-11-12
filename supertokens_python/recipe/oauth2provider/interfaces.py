59109a8d7caa53c521a86490f6d1339c06ee76e2
Sattvik Chakravarthy <sattvik@gmail.com>
2024-11-12 08:31:53 +0530

    fix: interface

--- supertokens_python/recipe/oauth2provider/interfaces.py
+++ supertokens_python/recipe/oauth2provider/interfaces.py
@@ -11,3 +11,38 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+from abc import ABC
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from supertokens_python.framework import BaseRequest, BaseResponse
+
+    from .utils import OAuth2ProviderConfig
+
+
+class RecipeInterface(ABC):
+    def __init__(self):
+        pass
+
+
+class APIOptions:
+    def __init__(
+        self,
+        request: BaseRequest,
+        response: BaseResponse,
+        recipe_id: str,
+        config: OAuth2ProviderConfig,
+        recipe_implementation: RecipeInterface,
+    ):
+        self.request: BaseRequest = request
+        self.response: BaseResponse = response
+        self.recipe_id: str = recipe_id
+        self.config: OAuth2ProviderConfig = config
+        self.recipe_implementation: RecipeInterface = recipe_implementation
+
+
+class APIInterface:
+    def __init__(self):
+        pass
