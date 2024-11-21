e91f461a0f2063a30a1b07cf115cb7784a559b05
Sattvik Chakravarthy <sattvik@gmail.com>
2024-11-21 17:41:44 +0530

    fix: update auth-react

--- .circleci/setupAndTestWithAuthReact.sh
+++ .circleci/setupAndTestWithAuthReact.sh
@@ -63,8 +63,48 @@ cd ../../../../supertokens-auth-react/
 # flag will not be checked because Auth0 is used as a provider so that the Thirdparty tests can run reliably. 
 # In versions lower than 0.18 Github is used as the provider.
 
-SKIP_OAUTH=true npm run test-with-non-node
-if [[ $? -ne 0 ]]
+# SKIP_OAUTH=true npm run test-with-non-node
+
+echo "Running tests with React 18"
+# Run node server in background.
+if [[ "${SERVER_STARTED}" != "true" ]]; then
+    (cd test/server/ && TEST_MODE=testing INSTALL_PATH=../../../supertokens-root NODE_PORT=8082 node . >> ~/test_report/react-logs/backend.log 2>&1 &)
+
+    (cd ./examples/for-tests/ && cat | CI=true BROWSER=none PORT=3031 REACT_APP_API_PORT=$apiPort npm run start >> ~/test_report/react-logs/frontend.log 2>&1 &)
+fi
+# Start front end test app and run tests.
+
+# Wait for the test app to be up before running tests.
+while ! curl -s localhost:3031 > /dev/null 2>&1
+do
+    echo "Waiting for front end test application to start..."
+    sleep 5
+done
+
+while ! curl -s localhost:8082 > /dev/null 2>&1
+do
+    echo "Waiting for backend test application to start..."
+    sleep 5
+done
+
+sleep 2 # Because the server is responding does not mean the app is ready. Let's wait another 2secs to make sure the app is up.
+
+
+mkdir -p ~/test_report/logs
+mkdir -p ~/test_report/react-logs
+mkdir -p ~/test_report/screenshots
+
+echo "Start mocha testing"
+
+if ! [[ -z "${CI}" ]]; then
+    export SPEC_FILES=$(circleci tests glob 'test/end-to-end/**/*.test.js' 'test/unit/**/*.test.js')
+    echo "Selected spec files: $SPEC_FILES, $CIRCLE_NODE_TOTAL/$CIRCLE_NODE_INDEX"
+fi
+
+SCREENSHOT_ROOT=~/test_report/screenshots APP_SERVER=$apiPort TEST_MODE=testing mocha --bail=$BAIL --require @babel/register --require test/test.mocha.env --timeout 40000 --no-config $SPEC_FILES
+testPassed=$?;
+
+if [[ $testPassed -ne 0 ]]
 then
     echo "test failed... killing $pid and exiting!"
     kill -9 $pid
