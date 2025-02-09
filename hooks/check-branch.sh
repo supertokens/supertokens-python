bc3822850c36770ed080625af88805adcc46878c
Namit Nathwani <namsnath@gmail.com>
2025-02-10 01:52:49 +0530

    update: adds hook to prevent commits to master

--- hooks/check-branch.sh
+++ hooks/check-branch.sh
@@ -2,14 +2,7 @@
 
 source ./hooks/populate-hook-constants.sh
 
-isMasterBranch=$( if [ "$currentBranch" = "master" ]; then echo true; else echo false; fi )
 isVersionBranch=$( if [[ ${currentBranch} =~ ^[0-9]+.[0-9]+$ ]]; then echo true; else echo false; fi )
-
-if "$isMasterBranch"
-then
-    echo "Committing to master, unexpected."
-    exit 1
-fi
 
 # Check that the code version matches the branch version if on a versioned branch
 # `%.*` strips out the suffix after (and including) the last `.`
