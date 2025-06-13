d2a0ba48bcdcb805bc399a3fc6efbd7afe6ae198
Namit Nathwani <namsnath@gmail.com>
2025-06-13 16:48:20 +0530

    feat: dev and release pipelines (#590)
    * feat: adds draft of workflows
    
    * update: enable auth-react
    
    * feat: migrates dev and release tag steps
    
    * fix: mark as success http method
    
    * feat: setup testable pipelines
    
    * fix: minor issues with workflows
    
    * update: move tests to commit, simplify pipelines
    
    * update: backend-sdk-testing to match webauthn branch
    
    * update: remove placeholder step
    
    - Required only in workflow calls
    
    * update: enable steps, add option to skip tests check
    
    * fix: add steps to lint-code workflow
    
    * update: adds publish doc job
    
    * feat: adds app server log dir for website tests
    
    * Apply suggestions from code review
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * fix: remove backtick
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * fix: backticks, conditionals
    
    * update: use new PAT
    
    * feat: adds concurrency limits to test workflows
    
    - Ensures that only the last instance of the workflow on a ref runs
      - Cancels previous workflows
      - Useful in PRs where multiple commits might be pushed in succession
    
    * update: cleanup comment
    
    * fix: minor syntax issues
    
    * fix: minor syntax issues
    
    * update: docs workflow stage to prod
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- hooks/populate-hook-constants.sh
+++ hooks/populate-hook-constants.sh
@@ -4,17 +4,19 @@
 # - Abitrary Spaces: ` *`
 # - Extracting the version into a match group: `(...)`
 # - Substituting the matched string with the match group: `/\1/`
-constantsVersion=$(sed -n 's/^ *VERSION *= *["]\([0-9\.]*\).*/\1/p' supertokens_python/constants.py)
-setupVersion=$(sed -n 's/ *version *= *["]\([0-9\.]*\).*/\1/p' setup.py )
+export constantsVersion=$(sed -n 's/^ *VERSION *= *["]\([0-9\.]*\).*/\1/p' supertokens_python/constants.py)
+export constantsVersionXy=$(sed -n 's/^ *VERSION *= *["]\([0-9]*\.[0-9]*\).*/\1/p' supertokens_python/constants.py)
+export setupVersion=$(sed -n 's/ *version *= *["]\([0-9\.]*\).*/\1/p' setup.py )
+export setupVersionXy=$(sed -n 's/ *version *= *["]\([0-9]*\.[0-9]*\).*/\1/p' setup.py )
 
-newestVersion=$( if [[ "$constantsVersion" > "$setupVersion" ]]; then echo "$constantsVersion"; else echo "$setupVersion"; fi )
+export newestVersion=$( if [[ "$constantsVersion" > "$setupVersion" ]]; then echo "$constantsVersion"; else echo "$setupVersion"; fi )
 
 # Target branch of the PR.
 # Ideally, this is all we want to check.
 if [[ "$GITHUB_BASE_REF" != "" ]]
 then
-    targetBranch="$GITHUB_BASE_REF"
+    export targetBranch="$GITHUB_BASE_REF"
 else # Fallback to current branch if not in a PR
-    targetBranch=$(git branch --show-current 2> /dev/null) || targetBranch="(unnamed branch)" # Get current branch
+    export targetBranch=$(git branch --show-current 2> /dev/null) || export targetBranch="(unnamed branch)" # Get current branch
 fi
-targetBranch=${targetBranch##refs/heads/}  # Remove refs/heads/ if present
+export targetBranch=${targetBranch##refs/heads/}  # Remove refs/heads/ if present
