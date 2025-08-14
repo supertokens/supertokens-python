a26706e992e726ae6eef1fb52e13327ea0db7b96
Namit Nathwani <namsnath@gmail.com>
2025-08-14 17:14:02 +0530

    update: version

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ exclude_list = [
 
 setup(
     name="supertokens_python",
-    version="0.30.2",
+    version="0.31.0",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.com",
